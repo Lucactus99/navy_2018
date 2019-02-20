7e6038aa3f9af23e0e83b3652d157fd00812b4b6
Thomas Renck <thomas.renck@epitech.eu>
2019-02-20 15:02:36 +0100

    parser works for 1 line

--- input_handler.c
+++ input_handler.c
@@ -17,18 +17,28 @@ void store_ship_coordinate(input_t *input, char **av)
 	int y[2] = {0, 0};
 	int i = 0;
 	int a = 0;
+	int tmp = 0;
 	int length = 0;
 
-	for (i; i < 10; i++) {
-		if (a == 0)
-			length = buffer[i] + 48;
-		if (a == 1) {
-			x[0] = buffer[i] - 55;
-			y[0] = buffer[i] - 48;
+	for (i; buffer[i] != '\n'; i++) {
+		if (a == 0 && buffer[i] != ':')
+			length = buffer[i] - 48;
+		if (a == 1 && buffer[i + 1] != ':' && tmp == 0) {
+			x[0] = buffer[i] - 64;
+			y[0] = buffer[i + 1] - 48;
+			tmp++;
+		}
+		if (a == 2 && buffer[i + 1] != ':' && tmp == 1) {
+			x[1] = buffer[i] - 64;
+			y[1] = buffer[i + 1] - 48;
+			tmp++;
 		}
 		if (buffer[i] == ':')
 			a++;
 	}
+	//printf("%d\n", length);
 	printf("%d\n", x[0]);
 	printf("%d\n", y[0]);
+	printf("%d\n", x[1]);
+	printf("%d\n", y[1]);
 }
