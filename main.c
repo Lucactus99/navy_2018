c56436f99cbb4c923c803cfd7e115f4eb65303fe
Thomas Renck <thomas.renck@epitech.eu>
2019-02-27 14:07:11 +0100

    adding error gestion in main for args nb

--- main.c
+++ main.c
@@ -14,11 +14,13 @@ int main(int ac, char **av)
     map_t *maps = malloc(sizeof(map_t));
     input_t *input = malloc(sizeof(input_t));
 
-    create_maps(maps);
-    if (ac == 2) {
+    if (ac < 2)
+    	return (84);
+    if (ac >= 2 && ac <= 4) {
+    	create_maps(maps);
         store_ship_coordinate(maps, av);
+    	display_map(maps->playerone_map);
+    	get_playerone_input(input);
     }
-    display_map(maps->playerone_map);
-    get_playerone_input(input);
     return (0);
 }
